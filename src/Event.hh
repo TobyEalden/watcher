693a83b6c313f6d4f5faba87dc8e3a6e40f21c74
tobyealden <toby.ealden@gmail.com>
2026-01-19 07:41:21 +0000

    Remove Node/N-API types from core watcher headers

--- src/Event.hh
+++ src/Event.hh
@@ -1,40 +1,25 @@
 #ifndef EVENT_H
 #define EVENT_H
 
-#include <string>
-#include <node_api.h>
-#include "wasm/include.h"
-#include <napi.h>
-#include <mutex>
 #include <map>
+#include <mutex>
 #include <optional>
-
-using namespace Napi;
+#include <string>
+#include <vector>
 
 struct Event {
   std::string path;
   bool isCreated;
   bool isDeleted;
-  Event(std::string path) : path(path), isCreated(false), isDeleted(false) {}
-
-  Value toJS(const Env& env) {
-    EscapableHandleScope scope(env);
-    Object res = Object::New(env);
-    std::string type = isCreated ? "create" : isDeleted ? "delete" : "update";
-    res.Set(String::New(env, "path"), String::New(env, path.c_str()));
-    res.Set(String::New(env, "type"), String::New(env, type.c_str()));
-    return scope.Escape(res);
-  }
+  explicit Event(std::string path) : path(std::move(path)), isCreated(false), isDeleted(false) {}
 };
 
 class EventList {
-public:
+ public:
   void create(std::string path) {
     std::lock_guard<std::mutex> l(mMutex);
     Event *event = internalUpdate(path);
     if (event->isDeleted) {
-      // Assume update event when rapidly removed and created
-      // https://github.com/parcel-bundler/watcher/issues/72
       event->isDeleted = false;
     } else {
       event->isCreated = true;
@@ -60,7 +45,7 @@ public:
   std::vector<Event> getEvents() {
     std::lock_guard<std::mutex> l(mMutex);
     std::vector<Event> eventsCloneVector;
-    for(auto it = mEvents.begin(); it != mEvents.end(); ++it) {
+    for (auto it = mEvents.begin(); it != mEvents.end(); ++it) {
       if (!(it->second.isCreated && it->second.isDeleted)) {
         eventsCloneVector.push_back(it->second);
       }
@@ -77,7 +62,7 @@ public:
   void error(std::string err) {
     std::lock_guard<std::mutex> l(mMutex);
     if (!mError.has_value()) {
-      mError.emplace(err);
+      mError.emplace(std::move(err));
     }
   }
 
@@ -91,11 +76,11 @@ public:
     return mError.value_or("");
   }
 
-private:
+ private:
   mutable std::mutex mMutex;
   std::map<std::string, Event> mEvents;
   std::optional<std::string> mError;
-  Event *internalUpdate(std::string path) {
+  Event *internalUpdate(std::string const &path) {
     auto found = mEvents.find(path);
     if (found == mEvents.end()) {
       auto it = mEvents.emplace(path, Event(path));
--- src/Watcher.hh
+++ src/Watcher.hh
@@ -2,29 +2,21 @@
 #define WATCHER_H
 
 #include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <string>
 #include <unordered_set>
-#include <set>
-#include <node_api.h>
+
+#include "DirTree.hh"
 #include "Glob.hh"
 #include "Event.hh"
-#include "Debounce.hh"
-#include "DirTree.hh"
-#include "Signal.hh"
-
-using namespace Napi;
 
 struct Watcher;
 using WatcherRef = std::shared_ptr<Watcher>;
 
-struct Callback {
-  Napi::ThreadSafeFunction tsfn;
-  Napi::FunctionReference ref;
-  std::thread::id threadId;
-};
-
 class WatcherState {
 public:
-    virtual ~WatcherState() = default;
+  virtual ~WatcherState() = default;
 };
 
 struct Watcher {
@@ -34,40 +26,73 @@ struct Watcher {
   EventList mEvents;
   std::shared_ptr<WatcherState> state;
 
-  Watcher(std::string dir, std::unordered_set<std::string> ignorePaths, std::unordered_set<Glob> ignoreGlobs);
-  ~Watcher();
+  Watcher(std::string dir, std::unordered_set<std::string> ignorePaths,
+          std::unordered_set<Glob> ignoreGlobs)
+      : mDir(std::move(dir)),
+        mIgnorePaths(std::move(ignorePaths)),
+        mIgnoreGlobs(std::move(ignoreGlobs)) {}
+
+  ~Watcher() = default;
 
   bool operator==(const Watcher &other) const {
-    return mDir == other.mDir && mIgnorePaths == other.mIgnorePaths && mIgnoreGlobs == other.mIgnoreGlobs;
+    return mDir == other.mDir && mIgnorePaths == other.mIgnorePaths &&
+           mIgnoreGlobs == other.mIgnoreGlobs;
   }
 
-  void wait();
-  void notify();
-  void notifyError(std::exception &err);
-  bool watch(Function callback);
-  bool unwatch(Function callback);
-  void unref();
-  bool isIgnored(std::string path);
-  void destroy();
+  void wait() {
+    std::unique_lock<std::mutex> lk(mMutex);
+    mCond.wait(lk);
+  }
 
-  static WatcherRef getShared(std::string dir, std::unordered_set<std::string> ignorePaths, std::unordered_set<Glob> ignoreGlobs);
+  void notify() {
+    std::unique_lock<std::mutex> lk(mMutex);
+    mCond.notify_all();
+  }
+
+  void notifyError(std::exception &err) { mEvents.error(err.what()); }
+
+  bool isIgnored(std::string path) {
+    for (auto it = mIgnorePaths.begin(); it != mIgnorePaths.end(); it++) {
+      auto dir = *it + DIR_SEP;
+      if (*it == path || path.compare(0, dir.size(), dir) == 0) {
+        return true;
+      }
+    }
+
+    auto basePath = mDir + DIR_SEP;
+    if (path.rfind(basePath, 0) != 0) {
+      return false;
+    }
+
+    auto relativePath = path.substr(basePath.size());
+    for (auto it = mIgnoreGlobs.begin(); it != mIgnoreGlobs.end(); it++) {
+      if (it->isIgnored(relativePath)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  static WatcherRef getShared(std::string dir,
+                              std::unordered_set<std::string> ignorePaths,
+                              std::unordered_set<Glob> ignoreGlobs) {
+    return std::make_shared<Watcher>(std::move(dir), std::move(ignorePaths),
+                                     std::move(ignoreGlobs));
+  }
 
 private:
   std::mutex mMutex;
   std::condition_variable mCond;
-  std::vector<Callback> mCallbacks;
-  std::shared_ptr<Debounce> mDebounce;
-
-  std::vector<Callback>::iterator findCallback(Function callback);
-  void clearCallbacks();
-  void triggerCallbacks();
 };
 
 class WatcherError : public std::runtime_error {
 public:
   WatcherRef mWatcher;
-  WatcherError(std::string msg, WatcherRef watcher) : std::runtime_error(msg), mWatcher(watcher) {}
-  WatcherError(const char *msg, WatcherRef watcher) : std::runtime_error(msg), mWatcher(watcher) {}
+  WatcherError(std::string msg, WatcherRef watcher)
+      : std::runtime_error(std::move(msg)), mWatcher(std::move(watcher)) {}
+  WatcherError(const char *msg, WatcherRef watcher)
+      : std::runtime_error(msg), mWatcher(std::move(watcher)) {}
 };
 
 #endif
